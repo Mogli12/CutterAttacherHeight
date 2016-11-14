5bf1fc31d3a318e426a8fbe65a99f1946fb5704d
Stefan Biedenstein <mbiedens@gmail.com>
2016-11-14 23:08:02 +0100

    mogliBase 3.13

--- FS17_CutterAttacher/mogliBase.lua
+++ FS17_CutterAttacher/mogliBase.lua
@@ -15,6 +15,8 @@
 -- 3.06 degrees support in globalsLoad
 -- 3.10 FS17 version
 -- 3.11 postLoad to load old savegame
+-- 3.12 getUiScale
+-- 3.13 getUiScale (2)
 
 -- Usage:  source(Utils.getFilename("mogliBase.lua", g_currentModDirectory));
 --         _G[g_currentModDirectory.."mogliBase"].newClass( "AutoCombine", "acParameters" )
@@ -144,6 +146,22 @@ else
 			end
 		end
 
+	--********************************
+	-- getUiScale
+	--********************************
+		function _newClass_.getUiScale()
+			-- compatibility < patch 1.3
+			if g_uiScale ~= nil and 0 < g_uiScale and g_uiScale < 2 then
+				return g_uiScale
+			end
+			-- patch 1.3 and above
+			local uiScale = 1.0
+			if g_gameSettings ~= nil and g_gameSettings.getValue ~= nil then
+					uiScale = Utils.getNoNil(g_gameSettings:getValue("uiScale"), 1.0)
+			end
+			return uiScale 
+		end
+		
 	--********************************
 	-- getText
 	--********************************
